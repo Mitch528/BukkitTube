52313e14d50d3893b71f49a7b30e54c757d9bd7e
Mitchell Kutchuk <mitch528@gmail.com>
2013-04-16 15:37:47 -0700

    Removed some unnecessary code

--- BukkitTube/src/main/java/org/mitch528/BukkitTube/api/video/FileVideo.java
+++ BukkitTube/src/main/java/org/mitch528/BukkitTube/api/video/FileVideo.java
@@ -2,8 +2,6 @@ package org.mitch528.BukkitTube.api.video;
 
 import java.io.File;
 
-import org.apache.commons.io.FileUtils;
-
 public class FileVideo extends Video
 {
 	
@@ -12,27 +10,4 @@ public class FileVideo extends Video
 		this.filename = file.getAbsolutePath();
 	}
 	
-	@Override
-	public void start()
-	{
-		
-		try
-		{
-			
-			File tmp = File.createTempFile(filename, ".tmp");
-			tmp.deleteOnExit();
-			
-			FileUtils.copyFile(new File(filename), tmp);
-			
-			filename = tmp.getAbsolutePath();
-			
-		}
-		catch (Exception e)
-		{
-			
-		}
-		
-		loop();
-		
-	}
 }
